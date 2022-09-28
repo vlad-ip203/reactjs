90247afcc4d2a6202c196ddc549cb83d3d968bec
MikronT <vlad.ivanov.if@gmail.com>
2022-09-28 12:04:33 +0300

    Reformat the code

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Hello, <strike>World</strike> React!
+                </p>
+                <a className="App-link"
+                   href="https://www.w3schools.com/react/react_getstarted.asp"
+                   target="_blank"
+                   rel="noopener noreferrer">
+                    Link to the tutorial used
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
