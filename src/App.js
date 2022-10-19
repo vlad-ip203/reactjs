4c8817f0468da1bcbbe7d3c93376e0c2adf3d119
MikronT <vlad.ivanov.if@gmail.com>
2022-10-19 10:06:01 +0300

    Update stylesheets

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
+        <div className="app">
+            <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo"/>
                 <p>
                     Hello, <strike>World</strike> React!
                 </p>
-                <a className="App-link"
+                <a className="app-link"
                    href="https://www.w3schools.com/react/react_getstarted.asp"
                    target="_blank"
                    rel="noopener noreferrer">
