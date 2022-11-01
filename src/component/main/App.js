4f7998660c429d62e12e8efc8c76b391552b6299
MikronT <vlad.ivanov.if@gmail.com>
2022-11-01 11:42:24 +0200

    Add test column layout

--- src/component/main/App.js
+++ src/component/main/App.js
@@ -1,20 +1,23 @@
 import {Container} from "react-bootstrap";
+import Masonry from "react-masonry-css";
 
+
+const breakpointCols = {
+    default: 5,
+    1600: 4,
+    1200: 3,
+    800: 2,
+    400: 1
+}
 
 function App() {
     return (
-            <Container>
-                <p>
-                    Hello, <strike>World</strike> React!
-                    <br/>
-                    <a className="link"
-                       href="https://www.w3schools.com/react/react_getstarted.asp"
-                       target="_blank"
-                       rel="noopener noreferrer">
-                        Link to the tutorial used
-                    </a>
-                </p>
-            </Container>
+        <Container>
+            <Masonry breakpointCols={breakpointCols}
+                     className='masonry-grid'
+                     columnClassName='masonry-grid-column'>
+            </Masonry>
+        </Container>
     );
 }
 
